be0e3f9f8cb40f9ab11dbd8875a87c01b1e2707a
Anish Ansari <anishansari@users.noreply.github.com>
2025-06-28 06:57:43 +0545

    Setup

---
+++ WebApplication3/Default.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="Home Page" Language="VB" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.vb" Inherits="WebApplication3._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:TextBox ID="text1" runat="server"></asp:TextBox><br /><br />
+
+    <asp:Button ID="Submit1" runat="server" Text="Add to Session State" OnClick="Session_Add" /><br /><br />
+    <asp:Button ID="Submit2" runat="server" Text="View Session State" OnClick="CheckSession" /><br /><br />
+
+    <asp:Label ID="span1" runat="server" Font-Bold="true" ForeColor="DarkBlue" />
+</asp:Content>
---
+++ WebApplication3/Default.aspx.vb
@@ -0,0 +1,20 @@
+﻿Public Class _Default
+    Inherits Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub Session_Add(sender As Object, e As EventArgs)
+        Session("MySession") = text1.Text
+        span1.Text = "Session data updated! <br />Your session contains: <font color='red'>" & Session("MySession").ToString() & "</font>"
+    End Sub
+
+    Protected Sub CheckSession(sender As Object, e As EventArgs)
+        If Session("MySession") Is Nothing OrElse Session("MySession").ToString() = "" Then
+            span1.Text = "NOTHING, SESSION DATA LOST"
+        Else
+            span1.Text = "Your session contains: <font color='red'>" & Session("MySession").ToString() & "</font>"
+        End If
+    End Sub
+End Class
